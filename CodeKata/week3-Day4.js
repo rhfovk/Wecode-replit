db3859e43f30b8578e0425bd859769a963026825
rhfovk <rhfovk@gmail.com>
2021-09-07 09:25:35 +0900

    ✨ Add: CodeKata week3-Day2,4 Update

---
+++ CodeKata/week3-Day4.js
@@ -0,0 +1,19 @@
+/* 문제
+주어진 숫자 배열에서, 0을 배열의 마지막쪽으로 이동시켜주세요. 원래 있던 숫자의 순서는 바꾸지 말아주세요.
+
+(새로운 배열을 생성해서는 안 됩니다.)
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+*/
+
+const moveZeroes = nums => {
+    for(i = nums.length-1; i >= 0; i--){
+
+    //반복문을 통해 0인것을 찾고 push를 통해 뒷쪽으로 넣어준다.
+      if(nums[i]===0){
+        nums.push(nums.splice(i, 1)[0]);
+      }
+    }
+    return nums
+  };
